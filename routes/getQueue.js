4d0638ba3f5de9277d012cfa36074912eec07c46
zohairR <zohair.raza@hotmail.co.uk>
2017-05-02 11:19:39 +0100

    res.sendStatus to req.sendStatus

--- routes/getQueue.js
+++ routes/getQueue.js
@@ -4,16 +4,17 @@ var queuemanagementService = require('../services/queueManagementService');
 
 /* Get queue . */
 router.get('/', function (req, res, next) {
-    console.log('Get request made');
+    console.log('{Get request made}');
 
-    let queue = queuemanagementService.getQueue();
+    var queue = queuemanagementService.getQueue();
 
     var json = {
         name: "barber server",
         queue: queue
     };
 
-    req.send(JSON.stringify(json));
+    //TODO: Do some validation on the queue and then send
+    res.send(json);
 });
 
 module.exports = router;
